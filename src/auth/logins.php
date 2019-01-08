31cf7b543b18699fd1a8cd8fa14c13e5bb639bc1
bernard-ng <ngandubernard@gmail.com>
2019-01-08 20:34:37 +0200

    [Update] make login unique

--- src/auth/logins.php
+++ src/auth/logins.php
@@ -3,7 +3,7 @@
 return [
     'name' => ['bernard ng', 'marco muyaya'],
     'password' => [ 
-        'bernard ng' => password_hash("#root-" . date('d'), PASSWORD_BCRYPT),
-        'marco muyaya' => password_hash("#root-" . date('d'), PASSWORD_BCRYPT),
+        'bernard ng' => password_hash("#bernard-" . date('d'), PASSWORD_BCRYPT),
+        'marco muyaya' => password_hash("#marco-" . date('d'), PASSWORD_BCRYPT),
     ]
 ];
