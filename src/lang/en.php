d1ec681102e3f262c96c88ac2746cf328d6ff35e
bernard-ng <ngandubernard@gmail.com>
2019-01-16 13:59:05 +0200

    [New] create lang files

---
+++ src/lang/en.php
@@ -0,0 +1,7 @@
+<?php
+
+// English lang for the app
+return [
+    'form.choose' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam iusto, ipsam eligendi accusamus reiciendis dignissimos necessitatibus saepe inventore sapiente! Porro harum rem ratione et repudiandae obcaecati id quae ducimus facere!',
+    'form.search' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam iusto, ipsam eligendi accusamus reiciendis dignissimos necessitatibus saepe inventore sapiente! Porro harum rem ratione et repudiandae obcaecati id quae ducimus facere!',
+];
